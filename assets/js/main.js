8339d37763d56af9e888e05aa21ed039a1e91443
karfaisarra <karfaisarra@gmail.com>
2022-10-28 17:39:26 +0200

    add methods 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,6 @@ const {createApp} =Vue
 createApp({
     data() {
       return {
-        container:{
-            text:'',
-            done: false,
-        },
         error: false,
         newTask:'',
         tasks:[
@@ -25,7 +21,7 @@ createApp({
             {
                 text:'Learn HTML',
                 done: false,
-            }
+            },
         ],
       }
     },
@@ -33,24 +29,37 @@ createApp({
         addTask(){
             console.log('ho cliccato');
             //console.log(this.newTask);
-            if (this.newTask.length < 5) {
+            if (this.newTask.trim().length < 5) {
                 //mostro messaggio
                 this.error = true
             }else{
                 this.error = false
                 //pusho la task nell'array
-                this.container.text =  this.newTask
-                this.tasks.unshift(this.container)
-                
+                //this.container.text =  this.newTask
+                const task = {
+                    text: this.newTask,
+                    done: false,
+                };
+                this.tasks.unshift(task) 
                 //svuatare l'input dopo che la task è stata aggiunta
                 this.newTask = ''
-
             }
-            this.container.text = ''
+            
         },
-        delete(index){
-            console.log('ho cliccato');
+        cancella(index){
+            console.log('ho cliccato su la task:', index);
             this.tasks.splice(index, 1)
+        },
+        sbarra(i){
+            console.log('ho cliccato');
+            const task = this.tasks[i]
+            if (task.done === false) {
+                task.done = true  
+            }else{
+                task.done= false
+            }
+            
         }
+        
     }
 }).mount('#app')
