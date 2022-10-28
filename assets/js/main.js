b8d1420c8fba76285553a99e3eb3a805db4bd44c
karfaisarra <karfaisarra@gmail.com>
2022-10-28 16:19:37 +0200

    add methods

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+const {createApp} =Vue
 
 createApp({
     data() {
       return {
-        todo:[
+        container:{
+            text:'',
+            done: false,
+        },
+        error: false,
+        newTask:'',
+        tasks:[
             {
                 text:'Learn JS',
                 done: false,
@@ -22,5 +28,29 @@ createApp({
             }
         ],
       }
+    },
+    methods: {
+        addTask(){
+            console.log('ho cliccato');
+            //console.log(this.newTask);
+            if (this.newTask.length < 5) {
+                //mostro messaggio
+                this.error = true
+            }else{
+                this.error = false
+                //pusho la task nell'array
+                this.container.text =  this.newTask
+                this.tasks.unshift(this.container)
+                
+                //svuatare l'input dopo che la task è stata aggiunta
+                this.newTask = ''
+
+            }
+            this.container.text = ''
+        },
+        delete(index){
+            console.log('ho cliccato');
+            this.tasks.splice(index, 1)
+        }
     }
 }).mount('#app')
